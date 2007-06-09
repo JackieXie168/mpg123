e40fd95dce92a6795779d95bcf7304683aad30f0
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2007-06-09 23:26:06 +0000

    tabsel is constant and internal to common.c

--- branches/mpg123lib/src/common.h
+++ branches/mpg123lib/src/common.h
@@ -45,7 +45,6 @@
 #define HDRCMPMASK 0xfffe0d00
 
 extern unsigned long firsthead;
-extern int tabsel_123[2][3][16];
 extern double compute_tpf(struct frame *fr);
 extern double compute_bpf(struct frame *fr);
 extern long compute_buffer_offset(struct frame *fr);
