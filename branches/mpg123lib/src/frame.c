1f208bfb481c94f79294c18920dbc44dbbdefb45
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2007-07-26 18:49:10 +0000

    Also nulling the bssave. Just to be safe.

--- branches/mpg123lib/src/frame.c
+++ branches/mpg123lib/src/frame.c
@@ -213,6 +213,7 @@ int frame_buffers_reset(struct frame *fr)
 	fr->bsbuf = fr->bsspace[1];
 	fr->bsbufold = fr->bsbuf;
 	memset(fr->bsspace, 0, 2*(MAXFRAMESIZE+512));
+	memset(fr->ssave, 0, 34);
 	memset(fr->rawbuffs, 0, fr->rawbuffss);
 	fr->hybrid_blc[0] = fr->hybrid_blc[1] = 0;
 	memset(fr->hybrid_block, 0, sizeof(real)*2*2*SBLIMIT*SSLIMIT);
