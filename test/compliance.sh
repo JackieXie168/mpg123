7fadb35dd24e9843826e7ce18037fe776482f571
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2009-04-30 23:09:52 +0000

    fix for new location, include the reference output in Makefile

--- test/compliance.sh
+++ test/compliance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-complbit="$(dirname $0)/MPEG1-layer3/compl.bit"
-compldouble="$(dirname $0)/MPEG1-layer3/compl.double"
+complbit="$(dirname $0)/compliance/layer3/compl.bit"
+compldouble="$(dirname $0)/compliance/layer3/compl.double"
 rms="$(dirname $0)/rmsdouble.bin"
 f32conv="$(dirname $0)/f32_double.bin"
 s16conv="$(dirname $0)/s16_double.bin"
