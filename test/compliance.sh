a02dcaa91f51fcbb32d241d79c3bf707f7e9dad3
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2009-05-10 18:00:25 +0000

    make compliance script more flexible, so that it works with ancient mpg123, too

--- test/compliance.sh
+++ test/compliance.sh
@@ -7,6 +7,16 @@ rms="$(dirname $0)/rmsdouble.bin"
 f32conv="$(dirname $0)/f32_double.bin"
 s16conv="$(dirname $0)/s16_double.bin"
 
+if [[ -e "$rms" ]] && [[ -e "$f32conv" ]] && [[ -e "$s16conv" ]]; then
+  true
+else
+  echo "The test programs are missing, please run 'make' in $(dirname $0)"
+fi
+
+nogap=
+$@ --longhelp 2>&1 | grep -q no-gapless && nogap=--no-gapless
+
+
 for lay in 1 2 3
 do
   echo
@@ -15,9 +25,11 @@ do
   eval "double=\$l$lay.double"
   if [[ -e "$bit" ]] && [[ -e "$double" ]]; then
     echo "layer $lay 16bit compliance (unlikely...)"
-$@ --no-gapless -q -e s16 -s "$bit" | "$s16conv" | "$rms" "$double" 2>/dev/null
+$@ $nogap -q  -s "$bit" | "$s16conv" | "$rms" "$double" 2>/dev/null
+    if $@ --longhelp 2>&1 | grep -q f32 ; then
     echo "layer $lay float compliance (hopefully fine)"
-$@ --no-gapless -q -e f32 -s "$bit" | "$f32conv" | "$rms" "$double" 2>/dev/null
+$@ $nogap -q -e f32 -s "$bit" | "$f32conv" | "$rms" "$double" 2>/dev/null
+    fi
   else
     echo "Layer $lay files are missing... do a make in the test dir!"
   fi
