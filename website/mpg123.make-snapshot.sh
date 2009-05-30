81207ad83bb93570a7c23ef86ea8206ef787f8ec
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2009-05-30 15:57:47 +0000

    announce svn fixes after 1.8rc2

--- website/mpg123.make-snapshot.sh
+++ website/mpg123.make-snapshot.sh
@@ -5,6 +5,7 @@ name=mpg123-$date
 base=/www/mpg123
 sdir=snapshot
 location=$base/$sdir/$name.tar.bz2
+compliance=$base/compliance.log
 
 echo workdir: $work
 cd $work &&
@@ -15,6 +16,17 @@ perl -pi -e 's:(AC_INIT\(\[mpg123\],\s+)\[[^\]]+\]:$1\['$date'\]:' configure.ac 
 svn2cl &&
 make distcheck &&
 mv $name.tar.gz .. &&
+./configure --disable-shared --enable-int-quality && make &&
+( echo "compliance test for $name" &&
+echo "First decoder in this list will be tested first, then generic: " &&
+src/mpg123 --test-cpu &&
+echo &&
+echo "Testing default decoder..." &&
+perl ~thomas/mpg123-test/compliance.pl src/mpg123 &&
+echo &&
+echo "Now the generic decoder:" &&
+perl ~thomas/mpg123-test/compliance.pl src/mpg123 --cpu generic
+) > $compliance &&
 cd .. &&
 gunzip $name.tar.gz &&
 bzip2 --best $name.tar &&
