883b071ed8204971c375b669b4855fadbe672bb2
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2012-07-26 14:06:00 +0000

    Include regression testing in snapshot preparation and website.

--- website/mpg123.make-snapshot.sh
+++ website/mpg123.make-snapshot.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 work=`mktemp -p /tmp -d mpg123-svn.XXXXX`
+
 date=$(date +%Y%m%d%H%M%S)
 name=mpg123-$date
 base=/www/mpg123
 sdir=snapshot
 location=$base/$sdir/$name.tar.bz2
 compliance=$base/compliance.log
+regression=$base/regression.log
 
-echo workdir: $work
+echo "Starting compliance tests ..." > $compliance
+echo "Starting regression tests ..." > $regression
+
+echo workdir: $workbase
 cd $work &&
+svn co svn://localhost/mpg123/test &&
+cd $work/test && make &&
 svn co svn://localhost/mpg123/trunk &&
 cd trunk &&
 perl -pi -e 's:(AC_INIT\(\[mpg123\],\s+)\[[^\]]+\]:$1\['$date'\]:' configure.ac &&
@@ -16,19 +23,21 @@ autoreconf -iv &&
 svn2cl &&
 ./configure &&
 make distcheck &&
-mv $name.tar.gz .. &&
-./configure --disable-shared --enable-int-quality && make &&
+mv $name.tar.gz $work &&
+./configure --prefix=$work/prefix --enable-int-quality && make && make install
 ( echo "compliance test for $name" &&
 echo "First decoder in this list will be tested first, then generic: " &&
-src/mpg123 --test-cpu &&
+$work/prefix/bin/mpg123 --test-cpu &&
 echo &&
 echo "Testing default decoder..." &&
-perl ~thomas/mpg123-test/compliance.pl src/mpg123 &&
+perl $work/test/compliance.pl $work/prefix/bin/mpg123 &&
 echo &&
 echo "Now the generic decoder:" &&
-perl ~thomas/mpg123-test/compliance.pl src/mpg123 --cpu generic
+perl $work/test/compliance.pl $work/prefix/bin/mpg123 --cpu generic
 ) > $compliance &&
-cd .. &&
+cd $work/test/regression &&
+MPG123_PREFIX=$work/prefix make &> $regression
+cd $work &&
 gunzip $name.tar.gz &&
 bzip2 --best $name.tar &&
 mv -v $name.tar.bz2 $location &&
