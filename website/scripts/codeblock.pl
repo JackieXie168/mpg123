21fc84bdf6058ea9226e963f43e4fe26c48bf463
thor <thor@35dc7657-300d-0410-a2e5-dc2837fedb53>
2016-01-31 10:08:51 +0000

    Better code filtering, avoid verbatim &.

--- website/scripts/codeblock.pl
+++ website/scripts/codeblock.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 
+# This filters <, >, and & out of text and prepends a special tag to each line,
+# rendering non-selectable line-numbers with the matching CSS code.
+
 my @lines = <>;
 
 my $digits = int(log(scalar @lines)/log(10)) + 1;
@@ -7,6 +10,8 @@ my $linefmt = sprintf('%%0%dd', $digits);
 my $i;
 for(@lines)
 {
+	# The three little replacements that make text harmless in HTML context.
+	s:\&:\&amp;:g; # That first, naturally.
 	s:<:\&lt;:g;
 	s:>:\&gt;:g;
 	print '<span class="num" data-num="'
